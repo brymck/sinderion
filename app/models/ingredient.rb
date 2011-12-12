a7e73399632ab559a954d78d105cff674748bd60
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-12-12 18:00:46 +0900

    Add several utility functions to ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,42 @@
 class Ingredient < ActiveRecord::Base
   has_and_belongs_to_many :effects
+
+  def compatible?(other_ingredient)
+    !matches(other_ingredient).blank?
+  end
+
+  def compatible_list(*ingredients)
+    unmatched = effects
+    matched = []
+
+    ingredients.each do |i|
+      new_matches = (unmatched & (i.effects - matched))
+      new_unmatches = i.effects - new_matches
+      matched += new_matches
+      unmatched += new_unmatches
+    end
+
+    Ingredient.all.keep_if do |i|
+      # Exclude any ingredients that are already mixed, plus any where there are
+      # no matches for effects unmatched thus far
+      !ingredients.include?(i) && (i != self) && !(i.effects & unmatched).blank?
+    end
+  end
+
+  def incompatible?(other_ingredient)
+    !compatible?(other_ingredient)
+  end
+
+  def matches(other_ingredient)
+    # Returns set intersection
+    effects & other_ingredient.effects
+  end
+
+  def nonmatches
+    (effects & other_ingredient.effects) - matches(other_ingredient)
+  end
+
+  def value
+    effects.map(&:value).inject(:+)
+  end
 end
